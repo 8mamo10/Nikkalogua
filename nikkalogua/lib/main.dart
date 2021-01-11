92c8e4a7592087e3e2057ee7ef1a19306c929859
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-01-11 13:57:15 +0900

    Use ListTile for menuitem

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -28,25 +28,22 @@ class MyApp extends StatelessWidget {
 }
 
 Widget _menuitem(String title, Icon icon) {
-  return GestureDetector(
-    child: Container(
-      padding: EdgeInsets.all(8.0),
-      decoration: new BoxDecoration(
-          border:
-              new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
-      child: Row(
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: icon,
-          ),
-          Text(title, style: TextStyle(color: Colors.black, fontSize: 18.0))
-        ],
+  return Container(
+    decoration: new BoxDecoration(
+        border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))),
+    child: ListTile(
+      leading: icon,
+      title: Text(
+        title,
+        style: TextStyle(color: Colors.black, fontSize: 18.0),
       ),
+      onTap: () {
+        print("onTap called");
+      },
+      onLongPress: () {
+        print("onLongPress called");
+      },
     ),
-    onTap: () {
-      print("onTap called");
-    },
   );
 }
 
