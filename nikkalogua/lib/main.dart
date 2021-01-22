96ca9e175f4bc12c4b74896257b6a96eb722afbc
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-01-22 21:19:49 +0900

    Remove unused import

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   debugPaintSizeEnabled = false;
