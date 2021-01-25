4eddd18b8e92ff322965d6ce3687a36845ed63d1
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-01-26 04:22:00 +0900

    Try BottomNavigationBar

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -17,7 +17,9 @@ class MyApp extends StatelessWidget {
       "pic3.jpg",
     ];
     return MaterialApp(
-        //home: RandomWords(),
+      //home: RandomWords(),
+      home: MainPage(),
+      /*
         home: Scaffold(
             appBar: AppBar(
               leading: Icon(Icons.menu),
@@ -99,12 +101,15 @@ class MyApp extends StatelessWidget {
                 // ),
                 //ChangeForm2(),
                 //ChangeForm3(),
-                ChangeForm4(),
-                ChangeForm5(),
-                ChangeForm6(),
-                ChangeForm7(),
+                //ChangeForm4(),
+                //ChangeForm5(),
+                //ChangeForm6(),
+                //ChangeForm7(),
               ],
-            )));
+            )
+            )
+            */
+    );
   }
 }
 
@@ -484,6 +489,66 @@ class _ChangeForm7State extends State<ChangeForm7> {
             child: new Text('select'),
           )
         ],
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  final _pageWidgets = [
+    PageWidget(color: Colors.white, title: 'Home'),
+    PageWidget(color: Colors.blue, title: 'Album'),
+    PageWidget(color: Colors.orange, title: 'Chat'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BottomNavigationBar'),
+      ),
+      body: _pageWidgets.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.photo_album), label: 'Photo'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+        ],
+        currentIndex: _currentIndex,
+        fixedColor: Colors.blueAccent,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) => setState(() => _currentIndex = index);
+}
+
+class PageWidget extends StatelessWidget {
+  final Color color;
+  final String title;
+
+  PageWidget({Key key, this.color, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
       ),
     );
   }
