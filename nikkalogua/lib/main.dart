c646cb077010056299e458eb779e0e3c969da985
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-02-03 20:35:37 +0900

    Try animation size change

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -22,7 +22,11 @@ class MyPage extends StatefulWidget {
 
 class _MyPageState extends State<MyPage> {
   Color _color = Colors.blue[100];
-  void _onTap() => setState(() => _color = Colors.blue[900]);
+  double _width = 100;
+
+  void _onTapColor() => setState(() => _color = Colors.blue[900]);
+  void _onTapSize() => setState(() => _width = 200);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -31,11 +35,27 @@ class _MyPageState extends State<MyPage> {
       body: AnimatedContainer(
         color: _color,
         duration: Duration(seconds: 1),
-        width: 100,
+        width: _width,
         height: 100,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _onTap,
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(
+              Icons.filter_1,
+              color: Colors.white,
+            ),
+            onPressed: _onTapColor,
+          ),
+          FloatingActionButton(
+            child: Icon(
+              Icons.filter_2,
+              color: Colors.white,
+            ),
+            onPressed: _onTapSize,
+          )
+        ],
       ),
     );
   }
