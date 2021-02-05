4cb0eca2677703b7f824da50df4964d6b190ae47
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-02-05 20:25:15 +0900

    Try animation alignment change

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -23,9 +23,11 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   Color _color = Colors.blue[100];
   double _width = 100;
+  Alignment _alg = Alignment.topLeft;
 
   void _onTapColor() => setState(() => _color = Colors.blue[900]);
   void _onTapSize() => setState(() => _width = 200);
+  void _onTapAlignment() => setState(() => _alg = Alignment.bottomRight);
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,10 +35,13 @@ class _MyPageState extends State<MyPage> {
         title: const Text('AnimatedContainer'),
       ),
       body: AnimatedContainer(
-        color: _color,
         duration: Duration(seconds: 1),
-        width: _width,
-        height: 100,
+        alignment: _alg,
+        child: Container(
+          width: _width,
+          height: 100,
+          color: _color,
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -54,6 +59,13 @@ class _MyPageState extends State<MyPage> {
               color: Colors.white,
             ),
             onPressed: _onTapSize,
+          ),
+          FloatingActionButton(
+            child: Icon(
+              Icons.filter_3,
+              color: Colors.white,
+            ),
+            onPressed: _onTapAlignment,
           )
         ],
       ),
