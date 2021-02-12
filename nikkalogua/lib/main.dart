6bf59fae301e30524b80bf5cf7bd80cdf5360408
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-02-12 22:09:15 +0900

    Remove unused import

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:nikkalogua/ClientsBloc.dart';
-import 'package:nikkalogua/Database.dart';
 import 'package:nikkalogua/ClientModel.dart';
 
 void main() {
