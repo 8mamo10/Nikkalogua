80f8666005b66ccaff2e5cf41d60983911afcc32
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-02-28 18:26:44 +0900

    Enable to move to next page by app bar

--- nikkalogua/lib/main.dart
+++ nikkalogua/lib/main.dart
@@ -7,50 +7,60 @@ import 'package:nikkalogua/ClientsPage.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   static const String _title = 'Nikkalogua';
-
-  var _nameList = <String>['str1', 'str2', 'str3', 'str4'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: Scaffold(body: ClientsPage()),
-/*
-        appBar: AppBar(
-          leading: Icon(Icons.copy),
-          title: const Text(_title),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              onPressed: () {},
+      home: CardListPage(),
+    );
+  }
+}
+
+class CardListPage extends StatefulWidget {
+  @override
+  _CardListPageState createState() => _CardListPageState();
+}
+
+class _CardListPageState extends State<CardListPage> {
+  var _nameList = <String>['str1', 'str2', 'str3', 'str4'];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.copy),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
             ),
-          ],
-        ),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => NextPage(
+                            paramText: 'name',
+                          )));
+            },
           ),
-          itemBuilder: (BuildContext context, int index) {
-            if (index == _nameList.length) {
-              return _cardPlus();
-            } else {
-              return _cardItem(context, _nameList[index]);
-            }
-          },
-          itemCount: _nameList.length + 1,
-        ),
+        ],
       ),
-      */
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == _nameList.length) {
+            return _cardPlus();
+          } else {
+            return _cardItem(context, _nameList[index]);
+          }
+        },
+        itemCount: _nameList.length + 1,
+      ),
     );
   }
 
