7e6d2b593e7f8b211ab1f48ab81a4f4168eebbb2
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-03-30 22:38:37 +0900

    Add nikka and log table

--- nikkalogua/lib/Database.dart
+++ nikkalogua/lib/Database.dart
@@ -1,4 +1,37 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DBProvider {
   DBProvider._();
   static final DBProvider db = DBProvider._();
+  static Database _database;
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, "Nikkalogua.db");
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute("CREATE TABLE nikka ("
+          "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+          "name TEXT,"
+          "color INTEGER,"
+          ")");
+      await db.execute("CREATE TABLE log ("
+          "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+          "nikka_id INTEGER,"
+          "date TEXT,"
+          ")");
+    });
+  }
 }
