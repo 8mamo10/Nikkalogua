1ceee04f2a368377a5812e89987e9d4cacc0d44b
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-05-23 20:52:37 +0900

    Enable to handle the entered name

--- nikkalogua/lib/EditPage.dart
+++ nikkalogua/lib/EditPage.dart
@@ -8,6 +8,9 @@ class EditPage extends StatefulWidget {
 class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
+    final _form = GlobalKey<FormState>();
+    String _nikkaName;
+
     return Stack(
       children: <Widget>[
         Container(
@@ -22,14 +25,26 @@ class _EditPageState extends State<EditPage> {
             elevation: 0.0,
           ),
           body: Form(
+            key: _form,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Text("習慣にしたいことは何ですか？"),
                 TextFormField(
-                  decoration: const InputDecoration(
+                  decoration: InputDecoration(
                     hintText: '習慣を入力',
                   ),
+                  onSaved: (value) {
+                    _nikkaName = value;
+                  },
+                ),
+                FlatButton(
+                  child: Text("完了"),
+                  color: Theme.of(context).buttonColor,
+                  onPressed: () {
+                    _form.currentState.save();
+                    print(_nikkaName);
+                  },
                 ),
               ],
             ),
