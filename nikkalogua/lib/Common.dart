d81d8badac634604a012696115478e34d044aa9d
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-06-06 12:20:40 +0900

    Add color

--- nikkalogua/lib/Common.dart
+++ nikkalogua/lib/Common.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
 const List colorTable = [
+  Colors.pink,
   Colors.red,
-  Colors.blue,
-  Colors.green,
-  Colors.yellow,
-  Colors.purple,
   Colors.orange,
+  Colors.yellow,
   Colors.lime,
+  Colors.lightGreen,
+  Colors.green,
+  Colors.lightBlue,
+  Colors.blue,
+  Colors.purple,
+  Colors.grey,
 ];
